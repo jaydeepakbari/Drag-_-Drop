723186bc2c507d7b85cdec2cdb7e8c7322bab2ac
jaydeepakbarigmailcom <jaydeepakbari@gmail.com>
2015-02-17 22:18:47 -0800

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -1,6 +1,6 @@
 <?php
 	if(count($_FILES) > 0)
 	{
-		move_uploaded_file($_FILES['img']['tmp_name'],"uploads/".$_FILES['img']['name']);
+		move_uploaded_file($_FILES['img']['tmp_name'],"./".$_FILES['img']['name']);
 	}	
 ?>
